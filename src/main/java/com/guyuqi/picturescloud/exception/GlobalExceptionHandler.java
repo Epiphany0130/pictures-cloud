b1852dea32f1e473e2097898fa3ffcdde23c19ed
Epiphany0130 <sdguyq@163.com>
2025-11-13 22:06:59 +0800

    初始化项目 - 全局异常处理器

---
+++ src/main/java/com/guyuqi/picturescloud/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+ package com.guyuqi.picturescloud.exception;
+
+import com.guyuqi.picturescloud.common.BaseResponse;
+import com.guyuqi.picturescloud.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
+
